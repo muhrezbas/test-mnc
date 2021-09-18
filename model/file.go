e8012085d9d7ba49f7dcbea0e378cb6e1b75ff16
muhrezbas <muhrezbasuki@gmail.com>
2021-09-19 00:18:50 +0700

    model

--- model/file.go
+++ model/file.go
@@ -1,23 +1,5 @@
 package model
 
-// FileInfo godoc
-type FileInfo struct {
-	Size       float64 `json:"size"`
-	SizeStr    string  `json:"size_string"`
-	Name       string  `json:"name"`
-	ModifiedAt string  `json:"modified_at"`
-}
-
-// DirInfo godoc
-type DirInfo struct {
-	Size       float64 `json:"size"`
-	SizeStr    string  `json:"size_string"`
-	Name       string  `json:"name"`
-	Type       string  `json:"type"`
-	IsDownload bool    `json:"is_download"`
-	ModifiedAt string  `json:"modified_at"`
-}
-
 // Nomor 2
 type Relation struct {
 	InfluencedBy  []string `json:"influenced-by"`
